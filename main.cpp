cb9a9e719ed15e555ad8d100c76c59b25c30b919
Naomi Estefanía <64105772+naominietov@users.noreply.github.com>
2020-05-22 08:35:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ Mayo 2020
 using namespace std;
 
 int main(){
-Hospitalizado paciente1("Becario","Aiuda", 2, "21/05/2020");
+Hospitalizado paciente1("Juan","Lopez", 2, "21/05/2020");
 cout<<"nombre: "<<paciente1.get_nombre()<<endl;
 cout<<"apellido: "<<paciente1.get_apellidos()<<endl;
 cout<<"dias de estancia: "<<paciente1.get_dest()<<endl;
@@ -29,4 +29,5 @@ Para este avance solo se implemento la herencia de paciente a la clase hospitali
 aunque para futuras entregas se crearán más, por lo que aqui solo se muestra paciente
 y su clase hija hospitalizado para futuras entregas se implementarán las demás clases
 incluyendo ambulatorio y las demás.
+Solo se crea un objeto de tipo hospitalizado.
 */
